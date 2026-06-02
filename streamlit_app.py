e4ec626b457f01b93cfe83f7dcf9091e44fe69d6
pavangiri <giripavan505@gmail.com>
2026-06-02 16:53:42 +0530

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("Customer Engagement & Product Utilization Analytics for Retention Strategy")
+
+st.header("Bank Customer Churn Analysis")
+
+st.write("""
+This dashboard analyzes customer engagement,
+product utilization, and churn retention behavior.
+""")
